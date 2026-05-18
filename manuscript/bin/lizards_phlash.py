5984a44f71a7678c1045850ace982b8bcb45fa73
Curro Campuzano <campuzanocurro@gmail.com>
2026-05-18 14:03:38 +0200

    Fix jax missing __main__

--- manuscript/bin/lizards_phlash.py
+++ manuscript/bin/lizards_phlash.py
@@ -17,28 +17,33 @@ import sys
 import numpy as np
 import phlash
 
-contigs_path = sys.argv[1]
-pkl_path = sys.argv[2]
-npz_path = sys.argv[3]
+def main():
+    contigs_path = sys.argv[1]
+    pkl_path = sys.argv[2]
+    npz_path = sys.argv[3]
 
-with open(contigs_path, "rb") as f:
-    contigs = pickle.load(f)
+    with open(contigs_path, "rb") as f:
+        contigs = pickle.load(f)
 
-# From https://academic.oup.com/mbe/article/39/1/msab311/6413643
-generation_time = 2
-mutation_rate_per_year = 1.98e-9
-mutation_rate = mutation_rate_per_year * generation_time
+    # From https://academic.oup.com/mbe/article/39/1/msab311/6413643
+    generation_time = 2
+    mutation_rate_per_year = 1.98e-9
+    mutation_rate = mutation_rate_per_year * generation_time
 
-test_data = contigs[0]
-train_data = contigs[1:]
-results = phlash.fit(data=train_data, test_data=test_data, mutation_rate=mutation_rate)
+    test_data = contigs[0]
+    train_data = contigs[1:]
+    results = phlash.fit(
+        data=train_data, test_data=test_data, mutation_rate=mutation_rate
+    )
 
-# Save fitted results
-with open(pkl_path, "wb") as f:
-    pickle.dump(results, f)
+    with open(pkl_path, "wb") as f:
+        pickle.dump(results, f)
 
-# Posterior Ne trajectories
-times = np.array([dm.eta.t[1:] for dm in results])
-T = np.geomspace(times.min(), times.max(), 1000)
-Nes = np.array([dm.eta(T, Ne=True) for dm in results])
-np.savez(npz_path, T=T, Nes=Nes)
+    times = np.array([dm.eta.t[1:] for dm in results])
+    T = np.geomspace(times.min(), times.max(), 1000)
+    Nes = np.array([dm.eta(T, Ne=True) for dm in results])
+    np.savez(npz_path, T=T, Nes=Nes)
+
+
+if __name__ == "__main__":
+    main()
